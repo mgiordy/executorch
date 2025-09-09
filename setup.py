4331fffe791075a90328a7446d92315b5fb9d6ad
Gregory Comer <gregoryjcomer@gmail.com>
2025-09-09 10:30:29 -0700

    [Windows] Add CI wheel build jobs for Windows
    ghstack-source-id: 133d02c05f88643ab024debe2c873e1a44b3f0fd
    ghstack-comment-id: 3240461597
    Pull-Request: https://github.com/pytorch/executorch/pull/13837

--- setup.py
+++ setup.py
@@ -142,6 +142,20 @@ class Version:
     @classmethod
     def write_to_python_file(cls, path: str) -> None:
         """Creates a file similar to PyTorch core's `torch/version.py`."""
+
+        print(f"Writing version.py to {path}, cwd: {os.getcwd()}", file=sys.stderr)
+
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+        try:
+            path1 = os.path.dirname(path)
+            path2 = os.path.dirname(path1)
+
+            print(f"Files at {path2}: {list(os.listdir(path2))}", file=sys.stderr)
+            print(f"Files at {path1}: {list(os.listdir(path1))}", file=sys.stderr)
+        finally:
+            pass
+
         lines = [
             "from typing import Optional",
             '__all__ = ["__version__", "git_version"]',
@@ -151,6 +165,9 @@ class Version:
         ]
         with open(path, "w") as fp:
             fp.write("\n".join(lines) + "\n")
+
+        file_dir = os.path.dirname(path)
+        print(f"Files: {list(os.listdir(file_dir))}", file=sys.stderr)
 
 
 # The build type is determined by the DEBUG environment variable. If DEBUG is
@@ -773,7 +790,7 @@ setup(
     # platform-specific files using InstallerBuildExt.
     ext_modules=[
         BuiltFile(
-            src_dir="%CMAKE_CACHE_DIR%/third-party/flatc_proj/bin/",
+            src_dir="%CMAKE_CACHE_DIR%/third-party/flatc_ep/bin/",
             src_name="flatc",
             dst="executorch/data/bin/",
             is_executable=True,
